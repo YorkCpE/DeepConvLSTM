1a469c654631da0a031139bc78bf3377e74d19a2
Jason Forsyth <jbforsyth@ycp.edu>
2018-04-04 13:04:50 -0400

    Update script to include all python notebook operation

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ import time
 
 import numpy as np
 import cPickle as cp
-import Theano.tensor as T
+import theano.tensor as T
 from sliding_window import sliding_window
 
 # Hardcoded number of sensor channels employed in the OPPORTUNITY challenge
@@ -61,6 +61,18 @@ def opp_sliding_window(data_x, data_y, ws, ss):
     return data_x.astype(np.float32), data_y.reshape(len(data_y)).astype(np.uint8)
 
 
+def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
+    assert len(inputs) == len(targets)
+    if shuffle:
+        indices = np.arange(len(inputs))
+        np.random.shuffle(indices)
+    for start_idx in range(0, len(inputs) - batchsize + 1, batchsize):
+        if shuffle:
+            excerpt = indices[start_idx:start_idx + batchsize]
+        else:
+            excerpt = slice(start_idx, start_idx + batchsize)
+        yield inputs[excerpt], targets[excerpt]
+
 ##### BEGIN MAIN ####
 
 print("Loading data...")
@@ -74,3 +86,52 @@ print(" ..after sliding window (testing): inputs {0}, targets {1}".format(X_test
 
 # Data is reshaped since the input of the network is a 4 dimension tensor
 X_test = X_test.reshape((-1, 1, SLIDING_WINDOW_LENGTH, NB_SENSOR_CHANNELS))
+
+net = {}
+net['input'] = lasagne.layers.InputLayer((BATCH_SIZE, 1, SLIDING_WINDOW_LENGTH, NB_SENSOR_CHANNELS))
+net['conv1/5x1'] = lasagne.layers.Conv2DLayer(net['input'], NUM_FILTERS, (FILTER_SIZE, 1))
+net['conv2/5x1'] = lasagne.layers.Conv2DLayer(net['conv1/5x1'], NUM_FILTERS, (FILTER_SIZE, 1))
+net['conv3/5x1'] = lasagne.layers.Conv2DLayer(net['conv2/5x1'], NUM_FILTERS, (FILTER_SIZE, 1))
+net['conv4/5x1'] = lasagne.layers.Conv2DLayer(net['conv3/5x1'], NUM_FILTERS, (FILTER_SIZE, 1))
+net['shuff'] = lasagne.layers.DimshuffleLayer(net['conv4/5x1'], (0, 2, 1, 3))
+net['lstm1'] = lasagne.layers.LSTMLayer(net['shuff'], NUM_UNITS_LSTM)
+net['lstm2'] = lasagne.layers.LSTMLayer(net['lstm1'], NUM_UNITS_LSTM)
+
+# In order to connect a recurrent layer to a dense layer, it is necessary to flatten the first two dimensions
+# to cause each time step of each sequence to be processed independently (see Lasagne docs for further information)
+net['shp1'] = lasagne.layers.ReshapeLayer(net['lstm2'], (-1, NUM_UNITS_LSTM))
+net['prob'] = lasagne.layers.DenseLayer(net['shp1'],NUM_CLASSES, nonlinearity=lasagne.nonlinearities.softmax)
+
+# Tensors reshaped back to the original shape
+net['shp2'] = lasagne.layers.ReshapeLayer(net['prob'], (BATCH_SIZE, FINAL_SEQUENCE_LENGTH, NUM_CLASSES))
+
+# Last sample in the sequence is considered
+net['output'] = lasagne.layers.SliceLayer(net['shp2'], -1, 1)
+
+# The model is populated with the weights of the pretrained network
+all_params_values = cp.load(open('weights/DeepConvLSTM_oppChallenge_gestures.pkl'))
+lasagne.layers.set_all_param_values(net['output'], all_params_values)
+
+# Compilation of theano functions
+# Obtaining the probability distribution over classes
+test_prediction = lasagne.layers.get_output(net['output'], deterministic=True)
+# Returning the predicted output for the given minibatch
+test_fn =  theano.function([ net['input'].input_var], [T.argmax(test_prediction, axis=1)])
+
+# Classification of the testing data
+print("Processing {0} instances in mini-batches of {1}".format(X_test.shape[0],BATCH_SIZE))
+test_pred = np.empty((0))
+test_true = np.empty((0))
+start_time = time.time()
+for batch in iterate_minibatches(X_test, y_test, BATCH_SIZE):
+    inputs, targets = batch
+    y_pred, = test_fn(inputs)
+    test_pred = np.append(test_pred, y_pred, axis=0)
+    test_true = np.append(test_true, targets, axis=0)
+
+
+# Results presentation
+print("||Results||")
+print("\tTook {:.3f}s.".format( time.time() - start_time))
+import sklearn.metrics as metrics
+print("\tTest fscore:\t{:.4f} ".format(metrics.f1_score(test_true, test_pred, average='weighted')))
